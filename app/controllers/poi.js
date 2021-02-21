c176514d702288949cb2b7ffa2605afb6e9dcfd5
Michael Kelly <20041540@mail.wit.ie>
2021-02-21 21:59:45 +0000

    After adding in the basic POI model and the ability to read and create.

--- app/controllers/poi.js
+++ app/controllers/poi.js
@@ -1,17 +1,31 @@
+'use strict';
+
+const Poidb = require('../models/poi-db');
+
 const POI = {
   home: {
     handler: function (request, h) {
       return h.view("home", { title: "Make a Donation" });
     },
   },
-  signup: {
-    handler: function (request, h) {
-      return h.file('./app/views/signup.hbs');
+  poiList: {
+    handler: async function (request, h) {
+      const pois = await Poidb.find().lean();
+      return h.view("poiList", {
+        title: "POI of Islands",
+        pois: pois,
+      });
     },
   },
-  login: {
-    handler: function (request, h) {
-      return h.file('./app/views/login.hbs');
+  poiAdd: {
+    handler: async function (request, h) {
+      const data = request.payload;
+      const newPoi = new Poidb({
+        name: data.name,
+        description: data.description,
+      });
+      await newPoi.save();
+      return h.redirect("/poiList");
     },
   },
 };
