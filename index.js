e682185535cb6eceda0df0cf8eecfbbba293d810
Michael Kelly <20041540@mail.wit.ie>
2021-02-25 21:29:55 +0000

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 'use strict';
 
-const env = require('dotenv');
-env.config();
 
 const Hapi = require('@hapi/hapi');
-const Inert = require("@hapi/inert");
-const Vision = require('@hapi/vision');
-const Handlebars = require('handlebars');
-const Cookie = require("@hapi/cookie");
+const dotenv = require('dotenv');
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
-  host: 'localhost',
 });
 
 require('./app/models/db');
 
 async function init() {
-  await server.register(Cookie);
-  await server.register(require('@hapi/Inert'));
-  await server.register(require('@hapi/Vision'));
-  server.views({
-    engines: {
-      hbs: require('handlebars'),
-    },
-    relativeTo: __dirname,
-    path: './app/views',
-    layoutPath: './app/views/layouts',
-    partialsPath: './app/views/partials',
-    layout: true,
-    isCached: false,
-  });
+  await server.register(require('@hapi/inert'));
+  await server.register(require('@hapi/vision'));
+  await server.register(require('@hapi/cookie'));
+
+  server.validator(require('@hapi/joi'))
 
   server.auth.strategy('session', 'cookie', {
     cookie: {
@@ -38,17 +29,29 @@ async function init() {
       password: process.env.cookie_password,
       isSecure: false
     },
-    redirectTo: "/",
+    redirectTo: '/'
   });
 
   server.auth.default('session');
+
+  server.views({
+    engines: {
+      hbs: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './app/views',
+    layoutPath: './app/views/layouts',
+    partialsPath: './app/views/partials',
+    layout: true,
+    isCached: false
+  });
 
   server.route(require('./routes'));
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 }
 
-process.on('unhandledRejection', (err) => {
+process.on('unhandledRejection', err => {
   console.log(err);
   process.exit(1);
 });
