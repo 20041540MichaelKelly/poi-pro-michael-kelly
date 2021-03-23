a0f9f8ab5e43476b1c312a8de8737915d14e6c4c
Michael Kelly <20041540@mail.wit.ie>
2021-03-23 16:22:17 +0000

    The final git push for this project, everything is completed i set out to do

--- app/controllers/gallery.js
+++ app/controllers/gallery.js
@@ -17,31 +17,6 @@ const Gallery = {
       }
     }
   },
-
-  uploadFile: {
-    handler: async function(request, h) {
-      try {
-        const file = request.payload.imagefile;
-        if (Object.keys(file).length > 0) {
-          await ImageStore.uploadImage(request.payload.imagefile);
-          return h.redirect('/pindex');
-        }
-        return h.view('gallery', {
-          title: 'Cloudinary Gallery',
-          error: 'No file selected'
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    payload: {
-      multipart: true,
-      output: 'data',
-      maxBytes: 209715200,
-      parse: true
-    }
-  },
-
   deleteImage: {
     handler: async function(request, h) {
       try {
